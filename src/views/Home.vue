581b39cd82437500c8e1560161c85e703405c169
mingjianglong <l502858283@gmail.com>
2023-08-24 17:04:39 +0800

    feat:增加异常处理

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home-container">
     {{ `${userInfo.userName}:` }}
     <Welcome />
+    <button @click="onButtonClick">点击</button>
   </div>
 </template>
 
@@ -9,7 +10,17 @@
 import {} from "vue"
 import userUserInfo from "@/store/useUserInfo"
 import Welcome from "@/components/Welcome.vue"
+import { UserDataError } from "exception_desc"
 const userInfo = userUserInfo()
+
+function onButtonClick() {
+  try {
+    
+  } catch (error) {
+    
+    throw new UserDataError({ message: "不能乱点击哈", code: 200 })
+  }
+}
 </script>
 
 <style scoped lang="less">
