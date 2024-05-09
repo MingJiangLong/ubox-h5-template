93bc86257cc3e53997ef0062452ecf7d9b5ba5a8
mingjianglong <l502858283@gmail.com>
2024-05-09 15:31:11 +0800

    feat: 移除无用内容

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,16 +10,10 @@
 import {} from "vue"
 import userUserInfo from "@/store/useUserInfo"
 import Welcome from "@/components/Welcome.vue"
-import { UserDataError } from "exception_desc"
 const userInfo = userUserInfo()
 
 function onButtonClick() {
-  try {
-    
-  } catch (error) {
-    
-    throw new UserDataError({ message: "不能乱点击哈", code: 200 })
-  }
+
 }
 </script>
 
